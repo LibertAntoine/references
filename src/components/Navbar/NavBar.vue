6ff6dcac019582b8fb2b7c2f733786a6c976700e
Antoine Libert <contact@antoine-libert.com>
2025-12-28 17:11:37 +0100

    feat: implement basic header navbar

---
+++ src/components/Navbar/NavBar.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="bg-slate-50 h-11 w-full flex items-center px-4 shadow-md">
+    <div class="flex items-center text-slate-700">
+      <BookMarked class="w-6 h-6 mr-1.5" />
+      <h1 class="text-lg font-semibold">Book references</h1>
+    </div>
+    <div class="ml-auto">
+      <PageThemeButton />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { BookMarked } from 'lucide-vue-next'
+
+import PageThemeButton from './PageThemeButton.vue'
+</script>
---
+++ src/components/Navbar/PageThemeButton.vue
@@ -0,0 +1,26 @@
+<template>
+  <button
+    class="group rounded-full p-[3px] transition-colors hover:bg-slate-200 dark:hover:bg-slate-800"
+    :title="isDark ? 'Light Mode' : 'Dark Mode'"
+    @click="() => toggleDark()"
+  >
+    <Sun
+      v-if="isDark"
+      class="h-[17px] w-[17px] text-slate-700 transition-colors group-hover:text-slate-900 dark:text-slate-200 dark:group-hover:text-slate-100"
+    />
+    <Moon
+      v-else
+      class="h-[17px] w-[17px] text-slate-700 transition-colors group-hover:text-slate-900 dark:text-slate-200 dark:group-hover:text-slate-100"
+    />
+  </button>
+</template>
+
+<script setup lang="ts">
+import { useDark, useToggle } from '@vueuse/core'
+import { Sun } from 'lucide-vue-next'
+
+import Moon from './MoonIcon.vue'
+
+const isDark = useDark()
+const toggleDark = useToggle(isDark)
+</script>
